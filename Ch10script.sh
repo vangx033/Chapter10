378f948f4dde52b0a4961c7af39466ecf6f08caa
vangx033 <seeho.vang@hotmail.com>
2018-03-28 18:35:40 -0700

    Ch10script.sh

--- Ch10script.sh
+++ Ch10script.sh
@@ -2,5 +2,21 @@
 #Xee Ho Vang
 #Chapter 10 script
 
-#telnet 
-telnet  google.com 
+# Utilizing the telnet command
+telnet www.google.com 
+
+echo "Viewing TCP packets and please press Ctrl C to exit"
+sudo tcpdump tcp
+
+sleep 5
+
+#Port scanning
+nmap 192.168.124.136
+sleep 5
+
+
+#Viewing a list of Unix domain
+lsof -U
+
+
+
